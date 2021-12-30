28b1ce1a29b5abffb51089c33f30983b35956f39
riya40 <priyanunna20@gmail.com>
2021-12-30 21:28:22 +0530

    Added:fndind the maximum in integers

---
+++ src/test/java/com/bl/generics/FindingLargestNumberTest.java
@@ -0,0 +1,17 @@
+package com.bl.generics;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+class FindingLargestNumberTest {
+    FindingLargestNumber findingLargestNumber = new FindingLargestNumber();
+
+    @Test
+    void testlargestfirstvalueshouldlarge() {
+        int a = findingLargestNumber.maximum(5, 4, 2);
+        Assertions.assertEquals(5, a);
+        System.out.println("test is done");
+    }
+
+   
+}
